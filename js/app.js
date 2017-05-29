11119fa2f6f97573c172f8ffa1efa8945f13383d
maryleo3007 <maryleo.3007@gmail.com>
2017-05-29 18:16:48 -0500

    add geolozalizacion

--- js/app.js
+++ js/app.js
@@ -1,24 +1,12 @@
 function initMap() {
       var uluru = {lat: -12.1190457, lng: -77.0408834};
       var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 5,
+        zoom: 16,
         center: uluru
       });
 
-
-
-window.addEventListener("load",function () {
-  var infoWindow = new google.maps.InfoWindow({map: map});
-  if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            == PackageManager.PERMISSION_GRANTED) {
-map.setMyLocationEnabled(true);
-} else {
-// Show rationale and request permission.
-}
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-})
-
+      var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+      });
 }
